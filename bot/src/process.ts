250161eaccfa3c40bcf42ce921fb1835bd2515f9
Kauã Berman Amorim <kauabamorim@outlook.com>
2023-07-18 17:15:20 -0300

    Update

--- bot/src/process.ts
+++ bot/src/process.ts
@@ -2,35 +2,41 @@ import puppeteer from "puppeteer";
 import parse from "node-html-parser";
 
 export const runPuppeteer = async () => {
+  const plates: string[] = ["PLA8614", "NYI8948"];
+  const renavams: string[] = ["01153252543", "01057242532"];
+
   try {
     // Inicializa
     // headless: faz com que abra o navegador
-    const browser = await puppeteer.launch({
-      headless: false,
-    });
 
-    const page = await browser.newPage();
-
-    await page.goto("http://smt.derba.ba.gov.br:8180/smt/home.action");
-
-    await page.waitForSelector("#renavam");
-
-    await page.type("#renavam", "01153252543");
-    await page.type("#placa", "PLA8614");
-
-    await page.click("#acao");
-
-    await page.waitForSelector("#paginacao");
-    const html = await page.content();
-    
-    const root = parse(html);
-
-    const infra = await page.waitForSelector('.descricaoAIT');
-    const value = await page.evaluate((el) => el.textContent, infra);
-
-    console.log(value)
-    
-    await browser.close();
+    for (let index = 0; index < 2; index++) {
+      const browser = await puppeteer.launch({
+        headless: false,
+      });
+  
+      const page = await browser.newPage();
+  
+      await page.goto("http://smt.derba.ba.gov.br:8180/smt/home.action");
+  
+      await page.waitForSelector("#renavam");
+  
+      await page.type("#renavam", renavams[index]);
+      await page.type("#placa", plates[index]);
+  
+      await page.click("#acao");
+  
+      await page.waitForSelector("#paginacao");
+      const html = await page.content();
+      
+      const root = parse(html);
+  
+      const infra = await page.waitForSelector('.descricaoAIT');
+      const value = await page.evaluate((el) => el.textContent, infra);
+  
+      console.log(value)
+      
+      await browser.close();
+    }
 
   } catch (error) {
     console.log(error);
