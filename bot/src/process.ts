7144e3571b72b6e740c37f9637eadb35cdb4d309
Kauã Berman Amorim <kauabamorim@outlook.com>
2023-07-18 23:50:06 -0300

    Update

--- bot/src/process.ts
+++ bot/src/process.ts
@@ -13,31 +13,51 @@ export const runPuppeteer = async () => {
       const browser = await puppeteer.launch({
         headless: false,
       });
-  
+
       const page = await browser.newPage();
-  
+
       await page.goto("http://smt.derba.ba.gov.br:8180/smt/home.action");
-  
+
       await page.waitForSelector("#renavam");
-  
+
       await page.type("#renavam", renavams[index]);
       await page.type("#placa", plates[index]);
-  
+
       await page.click("#acao");
-  
+
       await page.waitForSelector("#paginacao");
       const html = await page.content();
-      
+
       const root = parse(html);
-  
-      const infra = await page.waitForSelector('.descricaoAIT');
-      const value = await page.evaluate((el) => el.textContent, infra);
-  
-      console.log(value)
-      
+      const tableElement = root.querySelector(".descricaoAIT table");
+
+      if (tableElement) {
+        const cells = tableElement.querySelectorAll("td");
+        const numeroInfracao = root.querySelector('.numeroAIT')?.textContent?.trim() || '';
+        const dataHora = cells[1].textContent.trim();
+        const municipio = cells[5].textContent.trim();
+        const infracao = cells[7].textContent.trim();
+        const situacao = cells[9].textContent.trim();
+
+        console.log("Número da infração:", numeroInfracao);
+        console.log("Data e Hora:", dataHora);
+        console.log("Município:", municipio);
+        console.log("Infração:", infracao);
+        console.log("Situação:", situacao);
+      }
+
+      // const tabelaElemento = root.querySelector(".descricaoAIT table");
+      // if (tabelaElemento) {
+      //   const cells = tabelaElemento.querySelectorAll("td");
+      //   cells.forEach((cell, index) => {
+      //     const cellValue = cell.textContent.trim();
+      //     console.log(`Índice da célula: ${index}`);
+      //     console.log("Valor da célula:", cellValue);
+      //   });
+      // }
+
       await browser.close();
     }
-
   } catch (error) {
     console.log(error);
   }
