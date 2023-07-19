c5ec97028d7d4f1a0baa04199cefd207dd44e76f
Kauã Berman Amorim <kauabamorim@outlook.com>
2023-07-19 15:54:13 -0300

    Opening PDF

--- src/process.ts
+++ src/process.ts
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer";
 import parse from "node-html-parser";
+import { PDFExtract } from "pdf.js-extract";
 
 export const runPuppeteer = async () => {
   const plates: string[] = ["PLA8614", "NYI8948"];
@@ -8,7 +9,7 @@ export const runPuppeteer = async () => {
   try {
     // Inicializa
     // headless: faz com que abra o navegador
-    
+
     for (let index = 0; index < 2; index++) {
       const browser = await puppeteer.launch({
         headless: false,
@@ -32,24 +33,38 @@ export const runPuppeteer = async () => {
         await page.waitForSelector("#paginacao");
         const html = await page.content();
 
-        const root = parse(html);
-        const tableElement = root.querySelector(".descricaoAIT table");
+        const document = parse(html);
+        const paginacao = document.querySelector(".descricaoAIT table");
 
-        if (tableElement) {
-          const cells = tableElement.querySelectorAll("td");
-          const numberInfraction = root.querySelector(".numeroAIT")?.textContent?.trim() || "";
-          const datetime = cells[1].textContent.trim();
-          const county = cells[5].textContent.trim();
-          const infringement = cells[7].textContent.trim();
-          const situation = cells[9].textContent.trim();
+        let numberInfraction,
+          datetime,
+          county,
+          infringement,
+          situation = "";
 
-          console.log("========== Consulta 0" + [index + 1] + " ==========");
-          console.log("Número da infração:", numberInfraction);
-          console.log("Data e Hora:", datetime);
-          console.log("Município:", county);
-          console.log("Infração:", infringement);
-          console.log("Situação:", situation);
+        if (paginacao) {
+          const cells = paginacao.querySelectorAll("td");
+          numberInfraction =
+            document.querySelector(".numeroAIT")?.textContent?.trim() || "";
+          datetime = cells[1].textContent.trim();
+          county = cells[5].textContent.trim();
+          infringement = cells[7].textContent.trim();
+          infringement.split("<br>");
+          situation = cells[9].textContent.trim();
         }
+
+        const paginacaoPDF = document.querySelector('.botoesAIT a[title="Imprimir 2a Via"]');
+
+        if (paginacaoPDF) {
+          paginacaoPDF.removeAttribute("target");
+          await page.click('.botoesAIT a[title="Imprimir 2a Via"]');
+        }
+
+        console.log("========== Consulta 0" + [index + 1] + " ==========");
+        console.log("Número da infração:", numberInfraction + " " + infringement);
+        console.log("Data e Hora:", datetime);
+        console.log("Município:", county);
+        console.log("Situacao: ", situation);
       } else {
         const tituloNadaConstaElement = await page.waitForSelector(
           ".tituloNadaConsta"
@@ -57,9 +72,9 @@ export const runPuppeteer = async () => {
         if (tituloNadaConstaElement) {
           const html = await page.content();
 
-          const root = parse(html);
+          const document = parse(html);
 
-          const element = root
+          const element = document
             .querySelector(".tituloNadaConsta")
             ?.textContent.trim();
 
