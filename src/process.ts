f44c741b374f088e6e815fb61eef78975a068d11
Kauã Berman Amorim <kauabamorim@outlook.com>
2023-07-20 14:30:57 -0300

    Inject Script

--- src/process.ts
+++ src/process.ts
@@ -2,6 +2,8 @@ import puppeteer from "puppeteer";
 import parse from "node-html-parser";
 import { PDFExtract } from "pdf.js-extract";
 
+let document: any;
+
 export const runPuppeteer = async () => {
   const plates: string[] = ["PLA8614", "NYI8948"];
   const renavams: string[] = ["01153252543", "01057242532"];
@@ -33,8 +35,8 @@ export const runPuppeteer = async () => {
         await page.waitForSelector("#paginacao");
         const html = await page.content();
 
-        const document = parse(html);
-        const paginacao = document.querySelector(".descricaoAIT table");
+        const root = parse(html);
+        const paginacao = root.querySelector(".descricaoAIT table");
 
         let numberInfraction,
           datetime,
@@ -44,27 +46,33 @@ export const runPuppeteer = async () => {
 
         if (paginacao) {
           const cells = paginacao.querySelectorAll("td");
-          numberInfraction =
-            document.querySelector(".numeroAIT")?.textContent?.trim() || "";
+          numberInfraction = root.querySelector(".numeroAIT")?.textContent?.trim() || "";
           datetime = cells[1].textContent.trim();
           county = cells[5].textContent.trim();
-          infringement = cells[7].textContent.trim();
-          infringement.split("<br>");
+          infringement = cells[7].textContent.trim().split("br");
           situation = cells[9].textContent.trim();
         }
 
-        const paginacaoPDF = document.querySelector('.botoesAIT a[title="Imprimir 2a Via"]');
+        const paginacaoPDF = root.querySelector('.botoesAIT ul li a');
 
+        // Injetando Script (F12 Browser)
         if (paginacaoPDF) {
-          paginacaoPDF.removeAttribute("target");
-          await page.click('.botoesAIT a[title="Imprimir 2a Via"]');
+          await page.evaluate(() => {
+              const paginacao = document.querySelector(".botoesAIT ul li a");
+              paginacao.removeAttribute('target');
+          });
         }
 
+        await page.click('.botoesAIT a[title="Imprimir 2a Via"]');
+        
+        await page.waitForNavigation();
+        
         console.log("========== Consulta 0" + [index + 1] + " ==========");
         console.log("Número da infração:", numberInfraction + " " + infringement);
         console.log("Data e Hora:", datetime);
         console.log("Município:", county);
         console.log("Situacao: ", situation);
+
       } else {
         const tituloNadaConstaElement = await page.waitForSelector(
           ".tituloNadaConsta"
@@ -85,7 +93,7 @@ export const runPuppeteer = async () => {
         }
       }
 
-      await browser.close();
+      // await browser.close();
     }
   } catch (error) {
     console.log(error);
